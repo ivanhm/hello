0e77974630dcda7c971b0ad9b784c4f96957bf5c
ivanhm <ivanhm@users.noreply.github.com>
2017-08-17 15:22:03 -0400

    Update staging.sh

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -5,6 +5,8 @@ export ECS_CLUSTER=$ECS_CLUSTER_STG
 export ECS_SERVICE=$ECS_SERVICE_STG
 export SERVICE_CONTAINER_NAME=$ECS_SERVICE_STG
 export CONTAINER_IMAGE=280159221453.dkr.ecr.us-east-1.amazonaws.com/${ECS_SERVICE_NAME}:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
+export SERVICE_TOKEN=C11120AC-A8AF-43E0-8844-0CE3B50BEAE2
+export WISHABI_ENVIRONMENT=development
 
 #download latest deploy script
 aws s3 cp "s3://flipp-platform-development/deploy/platform-service-deploy.sh" platform-service-deploy.sh
